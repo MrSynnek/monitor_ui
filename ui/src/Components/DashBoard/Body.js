2c30cdd41042db20bc323a8ba326eb96720547a1
Ken <bccken@hotmail.com>
2021-06-29 15:45:47 +0700

    try fetch

--- ui/src/Components/DashBoard/Body.js
+++ ui/src/Components/DashBoard/Body.js
@@ -1,21 +1,42 @@
 import React, { useEffect, useState } from "react";
-import data from "./data"
 import IsSearch from "./ValidateSearch";
 import "./Dashboard.css";
 
 function Body(props) {
   const [input, setInput] = useState([]);
+  async function fetching(){
+    const requestOption = {
+      // mode: 'no-cors',
+      method: 'post',
+      headers: {"Content-Type":"application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Accept": "application/json"},
+      body: JSON.stringify({
+        deviceId: "mfmf",
+        userMode: "INDIVIDUAL",
+        configVersion: "8"
+    })
+    };
+    try{
+    const response = await fetch('http://localhost:11114/v1/toggle/flags',requestOption)
+    const data = await response.json();
+    setInput(data)
+    }
+    catch(error){
+      console.log("error while fetching data:",error)
+    }
+  }
   useEffect(() => {
-    setInput(data);
-  });
-
+  fetching();
+  },[]);
+  
   return (
     <>
         <div class="row bodyTable">
           <div className="tablehead row">
-            <div className="col-2 headcol" >Project</div>
-            <div className="col-2 headcol" >Service</div>
-            <div className="col-4 headcol" >Feature</div>
+            <div className="col-2 headcol" >Feature</div>
+            <div className="col-2 headcol" >Function</div>
+            <div className="col-4 headcol" >Feature Name</div>
             <div className="col-1 headcol" >Target</div>
             <div className="col-2 headcol" >Mode</div>
             <div className="col-1 headcol" >Edit</div>
